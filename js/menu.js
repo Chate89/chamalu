6b8e2282a5197dc692595af10b9f003fe69a0d44
Chate89 <gastonchatelet@gmail.com>
2019-02-04 12:50:27 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,20 @@ $(document).ready(main);
 
 var contador = 1;
 
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function () {
+        tilt([event.beta, event.gamma]);
+    }, true);
+} else if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function () {
+        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+    }, true);
+} else {
+    window.addEventListener("MozOrientation", function () {
+        tilt([orientation.x * 50, orientation.y * 50]);
+    }, true);
+}
+
 function main(){
 	$('.boton_menu').click(function(){
 
